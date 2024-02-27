984712fe326586553b43051939db26139aaf3298
vickyjayswal12 <vickyjayswal12@gmail.com>
2024-02-27 17:10:14 +0530

    handle nav according to user login or not

--- routs/user.js
+++ routs/user.js
@@ -7,12 +7,15 @@ rout.get("/signin",(req,resp)=>{
     resp.render('login')
 })
 rout.post("/signin",login_controller)
-rout.get('/singup',(req,resp)=>{
+rout.get('/signup',(req,resp)=>{
     resp.render('register')
 })
 rout.post('/signup',register_controller)
 
 rout.get('/profile',islogin,(req,resp)=>{
     resp.send("profile")
+})
+rout.get('/logout',(req,resp)=>{
+    resp.clearCookie("token").redirect('/')
 })
 module.exports=rout;
